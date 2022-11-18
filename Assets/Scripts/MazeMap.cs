c498104d6cb1f0a4633906b3aed52d9cea1d5f05
LOKESH PANTI - STUDENT <G00383507@atu.ie>
2022-11-18 18:42:35 +0000

    Started creating a mini random map generator and learnign what code goes into it.

--- Assets/Scripts/MazeMap.cs
+++ Assets/Scripts/MazeMap.cs
@@ -2,29 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum NodeState{
+public enum NodeState
+{
     Available,
     Current,
     Completed
 }
+
 public class MazeMap : MonoBehaviour
 {
-    //List of all the game objects...walls etc
     [SerializeField] GameObject[] walls;
     [SerializeField] MeshRenderer floor;
 
-    public void setState(NodeState state){
-        switch(state){
-             case NodeState.Available:
-             floor.material.color = Color.red;
-             break;
-             case NodeState.Current:
-             floor.material.color = Color.blue;
-             break;
-             case NodeState.Completed:
-             floor.material.color = Color.green;
-             break;
-            }
+    public void SetState(NodeState state)
+    {
+        switch (state)
+        {
+            case NodeState.Available:
+                floor.material.color = Color.white;
+                break;
+            case NodeState.Current:
+                floor.material.color = Color.blue;
+                break;
+            case NodeState.Completed:
+                floor.material.color = Color.green;
+                break;
+        }
     }
-
 }
