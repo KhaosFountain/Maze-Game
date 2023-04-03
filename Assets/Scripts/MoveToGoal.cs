66da55cfbd726e7b76917e52fb39a1489f045b58
LOKESH PANTI - STUDENT <g00383507@atu.ie>
2023-04-03 19:51:29 +0100

    changing the agtents code to where they get punished for not moving or taking actions

--- Assets/Scripts/MoveToGoal.cs
+++ Assets/Scripts/MoveToGoal.cs
@@ -15,24 +15,40 @@ using Unity.MLAgents.Sensors;
        - based on the decision it takes a action
    4. Reward
        - if the action is correct the agent gets a reward...if the action is incorrect then no reward.
-  */
+  
 public class MoveToGoal : Agent{
 
     [SerializeField] private Transform targetTransform;
+    [SerializeField] private Material winMaterial;
+    [SerializeField] private Material loseMaterial;
+    [SerializeField] private MeshRenderer floorMeshRenderer;
 
-    public override void CollectObservations(VectorSensor sensor){
-        sensor.AddObservation(transform.position);
-        sensor.AddObservation(targetTransform.position);
+
+    public override void OnEpisodeBegin(){
+        transform.localPosition = new Vector3(Random.Range(-50f, 50f), 0, Random.Range(-50f, 50f));
+        targetTransform.localPosition = new Vector3(Random.Range(-50f, 50f), 0, Random.Range(-50f, 50f));
     }
 
+    public override void CollectObservations(VectorSensor sensor){
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(targetTransform.localPosition);
+
+        // ray preception sensor has to take in observation of the goal if it is in line of sight......usually use
+        // tags to detect objects...e.g wall tag and goal tag. 
+
+        
+
+    }
 
     public override void OnActionReceived(ActionBuffers actions)
     {
         float moveX = actions.ContinuousActions[0];
-        float moveZ = actions.ContinuousActions[1];
+        float moveZ = actions.ContinuousActions[1]; 
 
-        float moveSpeed = 8f;
-        transform.position += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
+        float moveSpeed = 10f;
+        transform.localPosition += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
+
+
     }
 
     public override void Heuristic(in ActionBuffers actionsOut){
@@ -41,18 +57,111 @@ public class MoveToGoal : Agent{
        continiousActions[1] = Input.GetAxisRaw("Vertical");
     }
 
-    private void OnTriggerEnter(Collider other){
+    // private void OnTriggerEnter(Collider other){
+    //     if(other.TryGetComponent<Goal>(out Goal goal)){
+    //         SetReward(+3f);
+    //         EndEpisode();
+    //     }
+    //     else if (other.TryGetComponent<Wall>(out Wall wall)){
+    //         SetReward(-20f);
+    //         EndEpisode();
+    //     } 
+    // }
 
-        if(other.TryGetComponent<Goal>(out Goal goal)){
-            SetReward(+5f);
-            transform.position = new Vector3(0, 2, 0);
+    void OnCollisionEnter(Collision col){
+        if(col.gameObject.CompareTag("Goal") ){
+            SetReward(+10f);
+            Debug.Log("Hit Goal");
+            floorMeshRenderer.material = winMaterial;
+            EndEpisode();
         }
-        else if (other.TryGetComponent<Wall>(out Wall wall)){
-            SetReward(-10f);
-            transform.position = new Vector3(0, 2, 0);
-            
+        else if (col.gameObject.CompareTag("Wall")){
+            SetReward(-50f);
+            Debug.Log("Hit Wall");
+            floorMeshRenderer.material = loseMaterial;
+            EndEpisode();
         }
     }
 
 
+}*/
+
+
+
+
+
+public class MoveToGoal : Agent
+{
+    public float Movespeed = 10;
+    private Vector3 orig;
+    private GameObject Target = null;
+
+    private float degreePerSecond = 4;
+    private bool rotate = true;
+
+    [SerializeField] private Material winMaterial;
+    [SerializeField] private Material loseMaterial;
+    [SerializeField] private MeshRenderer floorMeshRenderer;
+
+    public override void Initialize()
+    { 
+        orig = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+        Target = this.transform.parent.transform.Find("Goal").gameObject;
+        
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        transform.localPosition = new Vector3(Random.Range(25f, -45f), 0, Random.Range(-44f, 24f));
+        Target.transform.localPosition = new Vector3(Random.Range(25f, -45f), 0, Random.Range(-44f, 24f));
+
+        timeSinceLastAction = 0f;
+    }
+    public override void OnActionReceived(ActionBuffers vectorAction)
+    {
+
+        float moveX = vectorAction.ContinuousActions[0];
+        float moveZ = vectorAction.ContinuousActions[1];
+
+        transform.localPosition += new Vector3(moveX, 0, moveZ) * Time.deltaTime * Movespeed;
+
+        // Reset time since last action
+        timeSinceLastAction = 0f;
+
+    }
+
+    public override void OnStepBegin(float timeSinceAction)
+    {
+        // Update time since last action
+        timeSinceLastAction += timeSinceAction;
+
+        // Punish for every second without action
+        if (timeSinceLastAction >= 1f)
+        {
+            SetReward(-0.1f);
+            timeSinceLastAction = 0f;
+        }
+    }
+    
+        void OnCollisionEnter(Collision col){
+        if(col.gameObject.CompareTag("Goal") ){
+            SetReward(+10f);
+            Debug.Log("Hit Goal");
+            floorMeshRenderer.material = winMaterial;
+            EndEpisode();
+        }
+        else if (col.gameObject.CompareTag("Wall")){
+            SetReward(-50f);
+            Debug.Log("Hit Wall");
+            floorMeshRenderer.material = loseMaterial;
+            EndEpisode();
+        }
+
+    }
 }
+
+
+
+
+
+
